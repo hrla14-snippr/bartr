02b3ac1f125d2ce6d9fcb9a0b149a5c4e9bb6f58
Keebin <kirikliu@gmail.com>
2017-05-11 20:11:51 -0700

    Add service transaction updating and accepting functionality in front end

--- client/src/components/EngageReqListEntries.js
+++ client/src/components/EngageReqListEntries.js
@@ -7,8 +7,8 @@ import swal from 'sweetalert';
 const EngageReqListEntries = (props) => {
   let currMessages = [];
   let currentEngagement = props.currentEngagement;
-  let receiverUnits;
-  let senderUnits;
+  let receiverUnits = currentEngagement.service_transaction.receiver_svc_units;
+  let senderUnits = currentEngagement.service_transaction.sender_svc_units;
 
   const changeReceiverUnits = (event, result) => {
     receiverUnits = result.value;
@@ -18,7 +18,36 @@ const EngageReqListEntries = (props) => {
     senderUnits = result.value;
   };
 
-  // const updateBartr
+  const updateBartr = (event, selectedEngagement) => {
+    event.preventDefault();
+    const config = {
+      headers: {'Authorization': 'Bearer ' + localStorage.id_token}
+    };
+    if (receiverUnits === 0 && senderUnits === 0) {
+      swal({
+        title: 'Unable to Submit',
+        text: 'No Bartr has been made.',
+        confirmButtonText: "Please make a Bartr first.",
+        type: 'warning'
+      })
+    } else {
+      axios.put(`${API_ENDPOINT}/api/services/transaction`, {
+        sender_svc_units: senderUnits,
+        receiver_svc_units: receiverUnits,
+        engagement_id: selectedEngagement.id
+      }, config)
+        .then((res) => {
+          console.log('Successfully updated transaction bartr.', res)
+          swal({
+            title: 'Bartr Updated!',
+            text: 'You\'ve updated the current bartr.',
+            confirmButtonText: 'Please await a response from the other party.',
+            type: 'success'
+          })
+        })
+        .catch((e) => console.log('Error updating bartr transaction', e));
+      }
+  };
 
   _.each(currentEngagement.messages, message => {
     currMessages = [...currMessages, message.message] 
@@ -45,35 +74,42 @@ const EngageReqListEntries = (props) => {
     const config = {
       headers: {'Authorization': 'Bearer ' + localStorage.id_token}
     };
-    // sender_svc_units: req.body.sender_svc_units,
-    // receiver_svc_units: req.body.receiver_svc_units
-    axios.put(`${API_ENDPOINT}/api/services/transaction`, {
-      sender_svc_units: senderUnits,
-      receiver_svc_units: receiverUnits,
-      engagement_id: selectedEngagement.id,
-      accepted: true
-    }, config)
-      .then((res) => {
-        console.log('Transaction accepted', res);
-        return axios.put(`${API_ENDPOINT}/api/engagements/${selectedEngagement.id}`, {
-          where: {
-            id: selectedEngagement.id
-          }
-        }, config)
-      })
-    .then(data => {
-      console.log('Engagement updated! ', data);
+    if (receiverUnits === 0 && senderUnits === 0) {
       swal({
-        title: 'Engagement Complete!',
-        text: 'We hope it was a pleasant exerience!',
-        confirmButtonText: "Check Past Engagements in the Menu",
-        type: 'success'
+        title: 'Unable to Submit',
+        text: 'No Bartr has been made.',
+        confirmButtonText: "Please make a Bartr first.",
+        type: 'warning'
       })
-      props.fetchEngagements(data.data);
-    })
-    .catch(err => {
-      console.log('Error with engagementCompleted: ', err);
-    })
+    } else {
+      axios.put(`${API_ENDPOINT}/api/services/transaction`, {
+        sender_svc_units: senderUnits,
+        receiver_svc_units: receiverUnits,
+        engagement_id: selectedEngagement.id,
+        accepted: true
+      }, config)
+        .then((res) => {
+          console.log('Transaction accepted', res);
+          return axios.put(`${API_ENDPOINT}/api/engagements/${selectedEngagement.id}`, {
+            where: {
+              id: selectedEngagement.id
+            }
+          }, config)
+        })
+      .then(data => {
+        console.log('Engagement updated! ', data);
+        swal({
+          title: 'Engagement Complete!',
+          text: 'We hope it was a pleasant exerience!',
+          confirmButtonText: "Check Past Engagements in the Menu",
+          type: 'success'
+        })
+        props.fetchEngagements(data.data);
+      })
+      .catch(err => {
+        console.log('Error with engagementCompleted: ', err);
+      })
+    }
   }
 
   return(
@@ -108,8 +144,18 @@ const EngageReqListEntries = (props) => {
           />
         </div>
       </Well>
+      {
+        (receiverUnits > 0 || senderUnits > 0)
+        ? <div>
+            <h4>Current Bartr:</h4>
+            <p>{`${currentEngagement.service_transaction.sender_svc_units} units of ${currentEngagement.sender.service.type} from ${currentEngagement.sender.name}`}</p>
+            <p>{`for ${currentEngagement.service_transaction.receiver_svc_units} units of ${currentEngagement.receiver.service.type} from ${currentEngagement.receiver.name}`}</p>
+          </div>
+        : <h4>No Bartr Made Yet.</h4>
+      }
       <br/>
-      <Button value={currentEngagement} onClick={() => {engagementCompleted(event, currentEngagement)}} bsStyle="primary">Completed?</Button>
+      <Button value={currentEngagement} onClick={() => {updateBartr(event, currentEngagement)}} bsStyle="primary">Update Bartr</Button>
+      <Button value={currentEngagement} onClick={() => {engagementCompleted(event, currentEngagement)}} bsStyle="primary">Accept Bartr</Button>
     </Well>
   )
 }
