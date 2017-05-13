ce2ff38af824127ea3d4677955ad42309aaf7535
kevinliu6102 <kirikliu@gmail.com>
2017-05-13 13:43:44 -0700

    Revert "Update: Profiles"

--- client/src/containers/ProfileContainer.js
+++ client/src/containers/ProfileContainer.js
@@ -3,8 +3,7 @@ import UserProfile from '../components/UserProfile';
 import * as authSelectors from '../auth/Auth0Selectors'
 
 const mapStateToProps = (state) => {
-  console.log('state big fella',state.routing.locationBeforeTransitions.pathname.split('/')[2]);
-  // console.log('props big fella',props);
+  
   return {
     profile: authSelectors.getProfile(state),
     error: authSelectors.getError(state),
@@ -13,6 +12,7 @@ const mapStateToProps = (state) => {
 
 // const mapDispatchToProps = (dispatch) => {
 
+// }
 
 const ProfileContainer = connect(mapStateToProps, null)(UserProfile);
 
